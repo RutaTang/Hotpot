f9dcbd7f80b6e71e498d713ab0a215fdfbf698b6
RutaTang <953259325@qq.com>
2021-05-26 13:36:23 -0600

    setup and upload to pypi

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='hotpot',
+    version='1.0.0',
+    author='Ruta Tang',
+    author_email="953259325@qq.com",
+    url="https://github.com/RutaTang/Hotpot",
+    description="A tiny and easy python web framework just for your fun! :)",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    install_requires=[
+        'werkzeug',
+        'cryptography',
+    ],
+    python_requires=">=3.6",
+)
